0645eb42c455b2b05f116db46a19cd2003d6a2cf
sathia27 <sathia2704@gmail.com>
2014-07-17 01:02:28 +0530

    refactoring code

--- lib/cover_page_generator/image.rb
+++ lib/cover_page_generator/image.rb
@@ -6,19 +6,18 @@ module CoverPageGenerator
       @title = title
       @author = author
       public_folder = File.expand_path('../../../public', __FILE__)
-      @font = "#{public_folder}/ta.ttf"
+      @font = "#{public_folder}/tam.ttf"
     end
-
-    def generate
-      pwd = Dir.pwd
-      img =  Magick::ImageList.new(@file_name)
-      canvas = Magick::ImageList.new
-      canvas.new_image(450, 700, Magick::TextureFill.new(img))
+    
+    def image_text
       text = Magick::Draw.new
       text.encoding = "Unicode"
       text.font = @font
       text.gravity = Magick::CenterGravity
-      @title  = @title.scan(/\S.{0,30}\S(?=\s|$)|\S+/).join("\n")
+      text
+    end
+
+    def annotate text, canvas
       text.annotate(canvas, 450,300,0,0, @title) {
         self.fill = '#fff'
         self.pointsize = 30
@@ -27,6 +26,16 @@ module CoverPageGenerator
         self.fill = '#fff'
         self.pointsize = 22
       }
+    end
+
+    def generate
+      pwd = Dir.pwd
+      img =  Magick::ImageList.new(@file_name)
+      canvas = Magick::ImageList.new
+      canvas.new_image(450, 700, Magick::TextureFill.new(img))
+      @title  = @title.scan(/\S.{0,30}\S(?=\s|$)|\S+/).join("\n")
+      text  = image_text
+      annotate text, canvas
       titlize = @title.gsub(" ", "_").gsub("/", "_").gsub("\n", "_")
       puts "Generating '#{(pwd + "/project_madurai/" + titlize + ".jpg")}'"
       canvas.write(pwd + "/project_madurai/" + titlize + ".jpg")
